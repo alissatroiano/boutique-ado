267dbf79f013149b9d28f9548be3e249cae23165
Alissa Troiano <alissatroianonyc@gmail.com>
2021-01-15 20:40:37 +0000

    Copy and paste source code to repair sorting script error

--- products/views.py
+++ products/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
 from django.db.models.functions import Lower
+
 from .models import Product, Category
 
 
@@ -43,12 +44,11 @@ def all_products(request):
                 messages.error(request, "You didn't enter any search criteria!")
                 return redirect(reverse('products'))
 
-            queries = Q(name__icontains=query) | Q(
-                description__icontains=query)
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
 
     current_sorting = f'{sort}_{direction}'
-
+    
     context = {
         'products': products,
         'search_term': query,
