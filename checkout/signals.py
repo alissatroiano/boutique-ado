5f5471b9d863b8cbadd2de2c69e4e9d1e47f2fca
Alissa Troiano <alissatroianonyc@gmail.com>
2021-01-15 20:43:03 +0000

    Add order summary to checkout success page

--- checkout/signals.py
+++ checkout/signals.py
@@ -14,5 +14,4 @@ def update_on_delete(sender, instance, **kwargs):
     """
     Update order total on lineitem delete
     """
-    print('delete signal received')
     instance.order.update_total()
