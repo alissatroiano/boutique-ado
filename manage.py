27aec1c63d27ff9259883580897503c7c84393b0
Alissa Troiano <alissatroianonyc@gmail.com>
2021-01-18 03:35:01 +0000

    Troubleshoot dango erros

--- manage.py
+++ manage.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
 import os
+
+import os
 import sys
 
 
 def main():
-    """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'boutique_ado.settings')
     try:
         from django.core.management import execute_from_command_line
