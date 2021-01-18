838ea9b4629062cf8c3911f29878d00da11e2515
Alissa Troiano <alissatroianonyc@gmail.com>
2021-01-18 18:46:41 +0000

    Add and test user profile page

--- profiles/views.py
+++ profiles/views.py
@@ -6,6 +6,7 @@ from .forms import UserProfileForm
 
 
 def profile(request):
+    """ Display the user's profile. """
     profile = get_object_or_404(UserProfile, user=request.user)
 
     if request.method == 'POST':
