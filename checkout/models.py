2c62676960910e1b44230dfa9e8641408360681e
Alissa Troiano <alissatroianonyc@gmail.com>
2021-01-18 04:54:25 +0000

    Repair error causing bug in checkout models

--- checkout/models.py
+++ checkout/models.py
@@ -13,7 +13,7 @@ from profiles.models import UserProfile
 class Order(models.Model):
     order_number = models.CharField(max_length=32, null=False, editable=False)
     user_profile = models.ForeignKey(
-        UserProfile, on_delete=models.SET_NULL, null=True, 
+        UserProfile, on_delete=models.SET_NULL, null=True,
         blank=True, related_name='orders')
     full_name = models.CharField(max_length=50, null=False, blank=False)
     email = models.EmailField(max_length=254, null=False, blank=False)
@@ -50,8 +50,8 @@ class Order(models.Model):
         self.order_total = self.lineitems.aggregate(Sum(
             'lineitem_total'))['lineitem_total__sum'] or 0
         if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
-            self.delivery_cost = self.order_total 
-            * settings.STANDARD_DELIVERY_PERCENTAGE / 100
+            self.delivery_cost = self.order_total * \
+                settings.STANDARD_DELIVERY_PERCENTAGE / 100
         else:
             self.delivery_cost = 0
         self.grand_total = self.order_total + self.delivery_cost
@@ -78,7 +78,7 @@ class OrderLineItem(models.Model):
     product_size = models.CharField(max_length=2, null=True, blank=True)
     quantity = models.IntegerField(null=False, blank=False, default=0)
     lineitem_total = models.DecimalField(
-        max_digits=6, decimal_places=2, 
+        max_digits=6, decimal_places=2,
         null=False, blank=False, editable=False)
 
     def save(self, *args, **kwargs):
