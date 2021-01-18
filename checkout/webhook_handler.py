f18b9db13348810da8e3fb9e3fb24448649f4562
Alissa Troiano <alissatroianonyc@gmail.com>
2021-01-18 22:05:01 +0000

    Repair typo in webhook_handler

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -16,7 +16,7 @@ class StripeWH_Handler:
 
     def __init__(self, request):
         self.request = request
-        
+
     def _send_confirmation_email(self, order):
         """Send the user a confirmation email"""
         cust_email = order.email
