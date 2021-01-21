9b79a52b4b9d848e053e22486b7126a6f5c0348a
Alissa Troiano <alissatroianonyc@gmail.com>
2021-01-21 20:23:45 +0000

    Fix secret key in environment

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 import dj_database_url
 
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -22,12 +21,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'k!*rdr976$l^xf3=7gp&jv01ipv*6opmwk%(rqmr2_pma8=zh+'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['boutique-ado-alissa.herokuapp.com', 'localhost']
+
 
 # Application definition
 
@@ -77,7 +77,8 @@ TEMPLATES = [
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
-                'django.template.context_processors.request',  # required by allauth
+                'django.template.context_processors.request', 
+                # required by allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'django.template.context_processors.media',
@@ -119,20 +120,9 @@ WSGI_APPLICATION = 'boutique_ado.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-'''
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-'''
-
-
 if 'DATABASE_URL' in os.environ:
-    DATABASES = {'default': dj_database_url.parse(
-        'postgres://mlrfwhsnackehn:49dd44ebd4a24b3f406847b970b4c351423b7f60ebf5c0ea3defbaec2e096195@ec2-52-71-231-180.compute-1.amazonaws.com:5432/d1f17727837rnf'
-    )
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
 else:
     DATABASES = {
@@ -140,54 +130,56 @@ else:
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
-
-        # Password validation
-        # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
-
-        AUTH_PASSWORD_VALIDATORS = [
-            {
-                'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-            },
-            {
-                'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-            },
-            {
-                'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-            },
-            {
-                'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-            },
-        ]
+    }
 
 
-        # Internationalization
-        # https://docs.djangoproject.com/en/3.0/topics/i18n/
+# Password validation
+# https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
-        LANGUAGE_CODE = 'en-us'
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
 
-        TIME_ZONE = 'UTC'
 
-        USE_I18N = True
+# Internationalization
+# https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-        USE_L10N = True
+LANGUAGE_CODE = 'en-us'
 
-        USE_TZ = True
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
 
 
-        # Static files (CSS, JavaScript, Images)
-        # https://docs.djangoproject.com/en/3.0/howto/static-files/
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-        STATIC_URL = '/static/'
-        STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
-        MEDIA_URL = '/media/'
-        MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-        # Stripe
-        FREE_DELIVERY_THRESHOLD = 50
-        STANDARD_DELIVERY_PERCENTAGE = 10
-        STRIPE_CURRENCY = 'usd'
-        STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
-        STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
-        STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
-        DEFAULT_FROM_EMAIL = 'boutiqueado@example.com'
+# Stripe
+FREE_DELIVERY_THRESHOLD = 50
+STANDARD_DELIVERY_PERCENTAGE = 10
+STRIPE_CURRENCY = 'usd'
+STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
+STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
+DEFAULT_FROM_EMAIL = 'boutiqueado@example.com'
